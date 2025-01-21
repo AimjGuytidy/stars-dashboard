3f0f6e04ed6216aff626992d5aac923d6d00cf5b
AimjGuytidy <aimjguy@gmail.com>
2025-01-21 16:05:29 +0200

    STARS Description

--- apps/app.py
+++ apps/app.py
@@ -5,12 +5,17 @@ import dash_html_components as html
 app = dash.Dash(__name__)
 
 #creating app layout:
-app.layout = html.Div([html.H1("Hello, World!",
+app.layout = html.Div([html.H1("Innovation for Poverty Actions Projects",
                                style={
                                    "color": "blue",
                                    "fontSize": "40px",
                                    "marginLeft": "20%"
-                               }),html.H1("checking what shows up")])
+                               }),
+                        html.H2("Supporting Teachers' Achievement in Rwandan Schools (STARS)"),
+                        html.P(["Key Facts:"]),
+                        html.Ul([html.Li(["Number of Activities: 3"]),html.Li(["Temporal Coverage: 2023-2024"]),
+                                 html.Li(["Last Updated: August 2, 2024"]), html.Li([html.A("STARS documentation",
+                                                                                            href="https://ipastorage.app.box.com/folder/170575516047?s=54ebq7bdudq022jmqg3xk13gju4p3f28")])])])
 
 # run the app
 if __name__ == "__main__":
