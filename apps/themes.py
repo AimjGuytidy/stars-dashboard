904dd550573415be550ccb1d498678e8d2aad0e7
AimjGuytidy <aimjguy@gmail.com>
2025-01-22 11:01:53 +0200

    improved themes app

--- apps/themes.py
+++ apps/themes.py
@@ -3,7 +3,7 @@ import dash_bootstrap_components as dbc
 import dash_html_components as html
 
 #app instantiation
-app = dash.Dash(__main__,external_stylesheets=[dbc.themes.MINTY])
+app = dash.Dash(__name__,external_stylesheets=[dbc.themes.MINTY])
 
 #app layout
 app.layout = html.Div([dbc.Col([html.H1("Innovation for Poverty Actions Projects",
