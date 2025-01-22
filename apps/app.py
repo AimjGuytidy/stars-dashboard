85d04b932ec5a40fec89000347b3b61a548622cf
AimjGuytidy <aimjguy@gmail.com>
2025-01-22 10:41:11 +0200

    adding colors

--- apps/app.py
+++ apps/app.py
@@ -1,17 +1,20 @@
 import dash
 import dash_html_components as html
+import dash_bootstrap_components as dbc
 
 #creating the app:
-app = dash.Dash(__name__)
+app = dash.Dash(__name__,external_stylesheets=[dbc.themes,MINTY])
 
 #creating app layout:
 app.layout = html.Div([html.H1("Innovation for Poverty Actions Projects",
                                style={
-                                   "color": "blue",
+                                   "color": "#155240",
                                    "fontSize": "40px",
                                    "marginLeft": "20%"
                                }),
-                        html.H2("Supporting Teachers' Achievement in Rwandan Schools (STARS)"),
+                        html.H2("Supporting Teachers' Achievement in Rwandan Schools (STARS)",
+                                style={"color": "#49AC57",
+                                       "fontSize": "25px"}),
                         html.P(["Key Facts:"]),
                         html.Ul([html.Li(["Number of Activities: 3"]),html.Li(["Temporal Coverage: 2023-2024"]),
                                  html.Li(["Last Updated: August 2, 2024"]), html.Li(["Source: ",html.A("STARS documentation",
